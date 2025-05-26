f71a909d2c6443b0ce6a93a07e54538630ad8e96
Côme Perrot <cperrot@laas.fr>
2025-05-26 15:41:56 +0200

    ext_template -> h1_extension

--- source/h1_extension/setup.py
+++ source/h1_extension/setup.py
@@ -1,4 +1,4 @@
-"""Installation script for the 'ext_template' python package."""
+"""Installation script for the 'h1_extension' python package."""
 
 import os
 import toml
@@ -18,8 +18,8 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="ext_template",
-    packages=["ext_template"],
+    name="h1_extension",
+    packages=["h1_extension"],
     author=EXTENSION_TOML_DATA["package"]["author"],
     maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
     url=EXTENSION_TOML_DATA["package"]["repository"],
