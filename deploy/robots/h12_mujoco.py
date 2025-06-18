b239f21c5538c9fa2487d85e82bce3cf83bf2c01
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-06-18 19:57:54 +0200

    Viewer threading in mujoco

--- deploy/robots/h12_mujoco.py
+++ deploy/robots/h12_mujoco.py
@@ -1,7 +1,9 @@
+import time
+import threading
+
 import mujoco
 import mujoco.viewer
 import numpy as np
-
 from h1_assets import SCENE_PATHS
 
 
@@ -13,14 +15,12 @@ class H1Mujoco:
         enable_GUI=False,
         fix_base=False,
     ):
-        self.enable_GUI = enable_GUI
-
         self.model = mujoco.MjModel.from_xml_path(scene_path)
         self.model.opt.integrator = 3
         self.model.opt.timestep = dt
         self.data = mujoco.MjData(self.model)
 
-        self.viewer = None
+        self.lock = threading.Lock()
 
         mujoco.mj_resetDataKeyframe(self.model, self.data, 0)
         self.qpos_des = self.data.qpos.copy()
@@ -37,6 +37,10 @@ class H1Mujoco:
 
         self.reset()
 
+        if enable_GUI:
+            thread = threading.Thread(target=self.run_render)
+            thread.start()
+
     def _set_config(self):
         self.kp = np.array([200, 200, 200, 300, 40, 40, 200, 200, 200, 300, 40, 40])
         self.kd = np.array([2.5, 2.5, 2.5, 4, 2, 2, 2.5, 2.5, 2.5, 4, 2, 2])
@@ -50,9 +54,9 @@ class H1Mujoco:
         for _ in range(self.decimation):
             torques = self._pd_control(q_ref)
             self._apply_torques(torques)
+            self.lock.acquire()
             mujoco.mj_step(self.model, self.data)
-            if self.enable_GUI:
-                self.render()
+            self.lock.release()
 
     def _apply_torques(self, torques):
         self.data.ctrl[:] = torques
@@ -69,17 +73,15 @@ class H1Mujoco:
             "base_angular_vel": self.data.qvel[3:6],
             "q_vel": self.data.qvel[6:],
         }
-        # return np.concatenate([self.data.qpos, self.data.qvel])
 
-    def render(self):
-        if self.viewer is None:
-            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
-        self.viewer.sync()
-
-    def close(self):
-        if self.viewer is not None:
-            self.viewer.close()
-            self.viewer = None
+    def run_render(self):
+        viewer = mujoco.viewer.launch_passive(self.model, self.data)
+        while viewer.is_running():
+            self.lock.acquire()
+            viewer.sync()
+            self.lock.release()
+            time.sleep(0.01)
+        viewer.close()
 
 
 if __name__ == "__main__":
