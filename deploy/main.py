e20c807641714cb503d7b1126ae1091a8e407259
Côme Perrot <cperrot@laas.fr>
2025-06-19 13:39:35 +0200

    Add exemple ONNX files

--- deploy/main.py
+++ deploy/main.py
@@ -6,13 +6,12 @@ from robots.h12_mujoco import H1Mujoco
 
 if __name__ == "__main__":
     scene_path = SCENE_PATHS["h12"]
-    sim = H1Mujoco(scene_path, enable_GUI=False)
+    sim = H1Mujoco(scene_path, enable_GUI=True, real_time=True)
 
     policy_path = str(Path(__file__).parent / "config" / "agent_model.onnx")
     policy = RLPolicy(policy_path)
 
     while True:
-
         state = sim.get_robot_state()
         q_ref = policy.step(state)
         sim.step(q_ref)
