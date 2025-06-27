5cba9c16c91c40bf2974e9066d3368bbd1c71fa4
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-06-27 16:34:56 +0200

    Logger with formatting

--- deploy/log.py
+++ deploy/log.py
@@ -4,10 +4,6 @@ from pathlib import Path
 
 import yaml
 from unitree_sdk2py.core.channel import ChannelFactoryInitialize, ChannelSubscriber
-from unitree_sdk2py.idl.default import (
-    unitree_hg_msg_dds__LowCmd_,
-    unitree_hg_msg_dds__LowState_,
-)
 from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowCmd_ as LowCmdHG
 from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowState_ as LowStateHG
 
@@ -18,9 +14,6 @@ class Logger:
 
         ChannelFactoryInitialize(0, config["net_interface"])
 
-        self.low_cmd = unitree_hg_msg_dds__LowCmd_
-        self.low_state = unitree_hg_msg_dds__LowState_
-
         self.lowcmd_subscriber = ChannelSubscriber("rt/lowcmd", LowCmdHG)
         self.lowcmd_subscriber.Init(self.low_cmd_handler, 10)
 
@@ -28,12 +21,15 @@ class Logger:
         self.lowstate_subscriber.Init(self.low_state_handler, 10)
 
     def low_cmd_handler(self, msg: LowCmdHG):
-        self.low_cmd = msg
-        logging.debug(f"low_cmd: {self.low_cmd}")
+        msg_str = ";".join([f"{i}:({cmd.q},{cmd.dq},{cmd.kp},{cmd.kd})" for i, cmd in enumerate(msg.motor_cmd)])
+        logging.debug("low_cmd:", msg_str)
 
     def low_state_handler(self, msg: LowStateHG):
-        self.low_state = msg
-        logging.debug(f"low_state: {self.low_state}")
+        state = list(msg.imu_state.quaternion)
+        state += list(msg.imu_state.gyroscope)
+        state += [(state.q, state.dq) for state in msg.motor_state]
+        msg_str = ";".join(map(str, state))
+        logging.debug("low_state:", msg_str)
 
 
 if __name__ == "__main__":
@@ -42,6 +38,8 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
     logger = Logger(config["real"])
-    while True:
-        time.sleep(1)
-        pass  # Keep the script running to continue logging
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stop logging")
