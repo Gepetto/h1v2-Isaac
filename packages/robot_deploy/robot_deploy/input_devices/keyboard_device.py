97116b3bd7773a7bdd210679be2880d310271029
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-11-04 17:50:33 +0100

    Add lock to keyboard input device

--- packages/robot_deploy/robot_deploy/input_devices/keyboard_device.py
+++ packages/robot_deploy/robot_deploy/input_devices/keyboard_device.py
@@ -1,4 +1,5 @@
 import numpy as np
+import threading
 
 import mujoco
 
@@ -9,30 +10,35 @@ class KeyboardDevice(InputDevice):
     def __init__(self) -> None:
         self.command = np.zeros(3)
         self.key_pressed = set()
+        self.lock = threading.Lock()
 
     def get_command(self) -> np.ndarray:
-        return self.command
+        with self.lock:
+            return self.command
 
     def is_pressed(self, *buttons: Button) -> bool:
-        return any(button in self.key_pressed for button in buttons)
+        with self.lock:
+            return any(button in self.key_pressed for button in buttons)
 
     def clear(self) -> None:
-        self.key_pressed.clear()
+        with self.lock:
+            self.key_pressed.clear()
 
     def key_callback(self, key) -> None:
         glfw = mujoco.glfw.glfw  # type: ignore
-        match key:
-            case glfw.KEY_UP | glfw.KEY_KP_8:
-                self.command[0] += 0.1
-            case glfw.KEY_DOWN | glfw.KEY_KP_5:
-                self.command[0] -= 0.1
-            case glfw.KEY_LEFT | glfw.KEY_KP_4:
-                self.command[1] += 0.1
-            case glfw.KEY_RIGHT | glfw.KEY_KP_6:
-                self.command[1] -= 0.1
-            case glfw.KEY_Z | glfw.KEY_KP_7:
-                self.command[2] += 0.1
-            case glfw.KEY_X | glfw.KEY_KP_9:
-                self.command[2] -= 0.1
-            case _:
-                self.key_pressed.add(glfw.get_key_name(key, 0))
+        with self.lock:
+            match key:
+                case glfw.KEY_UP | glfw.KEY_KP_8:
+                    self.command[0] += 0.1
+                case glfw.KEY_DOWN | glfw.KEY_KP_5:
+                    self.command[0] -= 0.1
+                case glfw.KEY_LEFT | glfw.KEY_KP_4:
+                    self.command[1] += 0.1
+                case glfw.KEY_RIGHT | glfw.KEY_KP_6:
+                    self.command[1] -= 0.1
+                case glfw.KEY_Z | glfw.KEY_KP_7:
+                    self.command[2] += 0.1
+                case glfw.KEY_X | glfw.KEY_KP_9:
+                    self.command[2] -= 0.1
+                case _:
+                    self.key_pressed.add(glfw.get_key_name(key, 0))
