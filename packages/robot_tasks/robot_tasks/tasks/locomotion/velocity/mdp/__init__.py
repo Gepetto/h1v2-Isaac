b747303976dcb2ebc675b49f30131d008803153d
Valentin-Guillet <guillet.valentin95240@gmail.com>
2026-02-24 12:34:52 +0100

    Remove unused files

--- packages/robot_tasks/robot_tasks/tasks/locomotion/velocity/mdp/__init__.py
+++
@@ -1,6 +0,0 @@
-"""This sub-module contains the functions that are specific to the locomotion environments."""
-
-from isaaclab.envs.mdp import *  # noqa: F403
-
-from .curriculums import *  # noqa: F403
-from .rewards import *  # noqa: F403
