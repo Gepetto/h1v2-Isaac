6c0a4041ab2111ac4f35014cc8e27a0dea067145
Valentin-Guillet <guillet.valentin95240@gmail.com>
2026-02-24 15:21:15 +0100

    Fix H12 27 training

--- packages/robot_tasks/robot_tasks/tasks/locomotion/velocity/config/h12_27dof/rough_env_cfg.py
+++ packages/robot_tasks/robot_tasks/tasks/locomotion/velocity/config/h12_27dof/rough_env_cfg.py
@@ -134,6 +134,37 @@ class H12RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
+        self.actions.joint_pos.joint_names = [
+            "left_hip_yaw_joint",
+            "left_hip_pitch_joint",
+            "left_hip_roll_joint",
+            "left_knee_joint",
+            "left_ankle_pitch_joint",
+            "left_ankle_roll_joint",
+            "right_hip_yaw_joint",
+            "right_hip_pitch_joint",
+            "right_hip_roll_joint",
+            "right_knee_joint",
+            "right_ankle_pitch_joint",
+            "right_ankle_roll_joint",
+            "torso_joint",
+            "left_shoulder_pitch_joint",
+            "left_shoulder_roll_joint",
+            "left_shoulder_yaw_joint",
+            "left_elbow_joint",
+            "left_wrist_roll_joint",
+            "left_wrist_pitch_joint",
+            "left_wrist_yaw_joint",
+            "right_shoulder_pitch_joint",
+            "right_shoulder_roll_joint",
+            "right_shoulder_yaw_joint",
+            "right_elbow_joint",
+            "right_wrist_roll_joint",
+            "right_wrist_pitch_joint",
+            "right_wrist_yaw_joint",
+        ]
+        self.actions.joint_pos.preserve_order = True
+
 
 @configclass
 class H12RoughEnvCfg_PLAY(H12RoughEnvCfg):
